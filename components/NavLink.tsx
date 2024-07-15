a7ea55c24773b6052449ede58f505f7cad4b898e
giovanni <giovidoh@gmail.com>
2024-07-15 20:13:50 +0000

    feat: Logo component added & NavLink component updated.

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const NavLink = ({ text, href }: { text: string; href: string }) => {
+const NavLink = ({
+    text = "NavLink",
+    href = "#",
+}: {
+    text: string;
+    href: string;
+}) => {
     return (
         <a id="ok" href={href} className="text-[#171717] font-semibold">
             {text}
