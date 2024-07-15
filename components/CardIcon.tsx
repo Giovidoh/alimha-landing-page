2fa054cc984d0a1391c4b723140648fd9ecd3d21
giovanni <giovidoh@gmail.com>
2024-07-15 23:27:20 +0000

    feat: CardIcon component added.

---
+++ components/CardIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+
+interface cardIconProps {
+    icon: StaticImageData;
+    bgColor: string;
+}
+
+const CardIcon: React.FC<cardIconProps> = ({ icon, bgColor }) => {
+    return (
+        <div
+            className={`${bgColor} flex justify-center items-center h-[88px] w-[88px] p-2 rounded-[28px]`}
+        >
+            <Image src={icon} alt="icon" />
+        </div>
+    );
+};
+
+export default CardIcon;
