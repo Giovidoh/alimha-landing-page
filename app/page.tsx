b9536af64805cbf26ae1b5d2850e239064571385
giovanni <giovidoh@gmail.com>
2024-07-16 20:36:07 +0000

    feat: first putting all together on landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import ContactUsSection from "@/components/ContactUsSection";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import OurServicesSection from "@/components/OurServicesSection";
+import TheyTrustUsSection from "@/components/TheyTrustUsSection";
+import WhoAreWeSection from "@/components/WhoAreWeSection";
+import WhyUsSection from "@/components/WhyUsSection";
+
 export default function Home() {
-    return <main className="h-full w-full"></main>;
+    return (
+        <main className="h-full w-full">
+            <Header />
+            <HeroSection />
+            <OurServicesSection />
+            <WhoAreWeSection />
+            <WhyUsSection />
+            <TheyTrustUsSection />
+            <ContactUsSection />
+            <Footer />
+        </main>
+    );
 }
