073c8b6a0ccaaf47fda637e0aec0c68632a596d1
giovanni <giovidoh@gmail.com>
2024-08-16 15:54:47 +0000

    feat: sendmo FaqSection added.

--- components/sendmo/Accordion.tsx
+++ components/sendmo/Accordion.tsx
@@ -1,33 +1,48 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
 import PlusIcon from "@/public/assets/plus-icon.svg";
 import MinusIcon from "@/public/assets/minus-icon.svg";
 
 interface AccordionProps {
     title?: string;
     body?: string;
-    active?: boolean;
 }
 
-const Accordion: FC<AccordionProps> = ({ title, body, active = false }) => {
+const Accordion: FC<AccordionProps> = ({ title, body }) => {
+    const [open, setOpen] = useState(false);
+
     return (
         <div
             className={`${
-                active
-                    ? "bg-secondary-blue px-14 py-5 w-full border border-secondary-blue rounded-xl"
-                    : "px-14 py-5 w-full border border-secondary-blue rounded-xl shadow-lg"
-            } `}
+                open
+                    ? " bg-secondary-blue px-14 py-5 w-full h-fit border border-secondary-blue rounded-xl transition-transform"
+                    : "w-full h-[80px] px-14 border py-5 border-secondary-blue rounded-xl shadow-lg"
+            }`}
         >
-            <div className="flex justify-between items-center">
-                <h2
-                    className={`text-xl font-bold ${
-                        active ? "text-white" : "text-black"
+            <button
+                className="flex justify-between items-center w-full h-full"
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                <span
+                    className={`text-xl text-start font-bold max-w-[70%] ${
+                        open ? "text-white" : "text-black"
                     }`}
                 >
                     {title}
-                </h2>
-                <button>{active ? <MinusIcon /> : <PlusIcon />}</button>
+                </span>
+
+                {open ? <MinusIcon /> : <PlusIcon />}
+            </button>
+
+            <div
+                className={`grid pt-2 transition-all duration-300 ease-in-out font-extralight text-white ${
+                    open
+                        ? "grid-rows-[1fr] opacity-100"
+                        : "grid-rows-[0fr] opacity-0"
+                }`}
+            >
+                <p className="overflow-hidden">{body}</p>
             </div>
-            {active && <div className="font-extralight text-white">{body}</div>}
         </div>
     );
 };
--- components/sendmo/sections/FaqSection.tsx
+++ components/sendmo/sections/FaqSection.tsx
@@ -2,6 +2,29 @@ import React from "react";
 import Badge from "../badges/Badge";
 import Accordion from "../Accordion";
 
+const accordionInfos = [
+    {
+        title: "Comment ouvrir un compte Sendmo",
+        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
+    },
+    {
+        title: "Sendmo est-elle facile à comprendre et à utiliser ?",
+        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
+    },
+    {
+        title: "Les utilisateurs de Sendmo ont-ils accès à un service d'assistance à la clientèle ?",
+        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
+    },
+    {
+        title: "Comment démarrer avec sendmo ?",
+        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
+    },
+    {
+        title: "Comment démarrer avec sendmo ?",
+        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
+    },
+];
+
 const FaqSection = () => {
     return (
         <section className="flex flex-col justify-between items-center gap-10 px-40 py-20">
@@ -14,11 +37,14 @@ const FaqSection = () => {
                     Quelques questions utiles pour l’utilisation de Sendmo.
                 </p>
             </div>
-            <div className="w-[80%]">
-                <Accordion
-                    title="Comment ouvrir un compte Sendmo"
-                    body="Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas."
-                />
+            <div className="flex flex-col w-[80%] gap-4">
+                {accordionInfos.map((accordion, index) => (
+                    <Accordion
+                        key={index}
+                        title={accordion.title}
+                        body={accordion.body}
+                    />
+                ))}
             </div>
         </section>
     );
