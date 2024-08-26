baa1787852e37d7ef3da1ee47b941f7d73c1c7eb
giovanni <giovidoh@gmail.com>
2024-08-26 16:34:19 +0000

    style: Alpay WhyUsSection responsivity done

--- components/alpay/sections/WhyUsSection.tsx
+++ components/alpay/sections/WhyUsSection.tsx
@@ -4,18 +4,26 @@ import Image from "next/image";
 import teamwork from "@/public/assets/teamwork.png";
 import businessMeeting from "@/public/assets/business-meeting.png";
 import dotVector1 from "@/public/assets/dot-vector-1.png";
+import linkedLines from "@/public/assets/linked-line-vectors.png";
 import FlexibilityIcon from "@/public/assets/flexibility-icon.svg";
 import InnovationIcon from "@/public/assets/innovation-icon.svg";
 import ClientSatisfactionIcon from "@/public/assets/client-satisfaction-icon.svg";
 
 const WhyUsSection = () => {
     return (
-        <section className="flex flex-row-reverse items-end w-full py-40 px-5 sm:px-20 min-[1120px]:px-40 gap-5">
-            <div className="relative flex flex-col w-1/2 gap-4">
+        <section className="relative flex flex-row-reverse items-end w-full py-10 min-[1000px]:py-24 px-5 sm:px-20 min-[1330px]:px-40 gap-5 overflow-hidden">
+            <div className="absolute bottom-[60%] left-0 rotate-180">
+                <Image
+                    src={linkedLines}
+                    alt="linked lines vector"
+                    width={1500}
+                />
+            </div>
+            <div className="flex flex-col justify-center items-center min-[1000px]:items-start w-full min-[1000px]:w-1/2 min-h-[600px] gap-4">
                 <span className="font-medium text-lg text-primary-blue">
                     Pourquoi-nous ?
                 </span>
-                <h1 className="font-bold text-4xl">
+                <h1 className="font-bold text-2xl sm:text-3xl min-[1000px]:text-4xl text-center min-[1000px]:text-start">
                     Pourquoi Choisir Notre Agrégateur
                 </h1>
                 <p className="text-sm text-[#292D32] mt-2 mb-5">
@@ -39,22 +47,22 @@ const WhyUsSection = () => {
                     />
                 </div>
             </div>
-            <div className="relative w-1/2">
-                <div className="absolute bottom-0 right-5">
+            <div className="relative hidden min-[1000px]:block w-1/2 h-[600px] z-10">
+                <div className="absolute top-10 right-5">
                     <Image
                         src={teamwork}
                         alt="phone computer image"
                         height={492}
                     />
                 </div>
-                <div className="absolute -bottom-24 left-0">
+                <div className="absolute bottom-0 right-[25%]">
                     <Image
                         src={businessMeeting}
                         alt="computer image"
                         height={263}
                     />
                 </div>
-                <div className="absolute left-16 bottom-56">
+                <div className="absolute top-40 right-[70%] -z-10">
                     <Image
                         src={dotVector1}
                         alt="dot vector image"
