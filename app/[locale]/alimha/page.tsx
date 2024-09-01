e001c87fd8a42b7a1a553b42d64e7bb8e1fd3f49
giovanni <giovidoh@gmail.com>
2024-09-01 03:51:04 +0000

    feat: animating logo ticker with framer-motion

--- app/[locale]/alimha/page.tsx
+++ app/[locale]/alimha/page.tsx
@@ -7,8 +7,10 @@ import TheyTrustUsSection from "@/components/alimha/sections/TheyTrustUsSection"
 import AboutUsSection from "@/components/alimha/sections/AboutUsSection";
 import WhyUsSection from "@/components/alimha/sections/WhyUsSection";
 import { Toaster } from "sonner";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+    const t = useTranslations("AlimhaPage");
     return (
         <main className="h-fit w-full">
             <Header />
@@ -16,7 +18,11 @@ export default function Home() {
             <OurServicesSection />
             <AboutUsSection />
             <WhyUsSection />
-            <TheyTrustUsSection />
+            <TheyTrustUsSection
+                tag={t("They trust us")}
+                title={t("Our partners and support")}
+                description={t("They trust us section description")}
+            />
             <ContactUsSection />
             <Footer />
             <Toaster
--- components/alimha/sections/TheyTrustUsSection.tsx
+++ components/alimha/sections/TheyTrustUsSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import circle_vector4 from "@/public/assets/circle-vector-4.png";
@@ -7,11 +8,20 @@ import soudhuLogo from "@/public/assets/soudhu-logo.png";
 import odeytech from "@/public/assets/Odeytech.jpg";
 import ubaLogo from "@/public/assets/uba-logo.png";
 import ecobankLogo from "@/public/assets/ecobank-logo.png";
-import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
+import { FC } from "react";
 
-const TheyTrustUsSection = () => {
-    const t = useTranslations("AlimhaPage");
+interface TheyTrustUsSectionProps {
+    tag: string;
+    title: string;
+    description: string;
+}
 
+const TheyTrustUsSection: FC<TheyTrustUsSectionProps> = ({
+    tag,
+    title,
+    description,
+}) => {
     return (
         <section className="relative flex flex-col justify-evenly items-center text-center px-[5%] lg:px-[10%] bg-white h-fit w-full pt-12 pb-24">
             <Image
@@ -34,44 +44,82 @@ const TheyTrustUsSection = () => {
                     height={50}
                 /> */}
                 <h3 className="font-semibold text-xl text-primary-blue pt-4">
-                    {t("They trust us")}
+                    {tag}
                 </h3>
             </div>
             <div className="mb-6">
                 <h1 className="font-bold text-[32px] xl:text-[44px] leading-[40px] xl:leading-[52.6px] pb-4">
-                    {t("Our partners & support")}
+                    {title}
                 </h1>
                 <p className="max-w-[595px] text-xl text-gray-1">
-                    {t("They trust us section description")}
+                    {description}
                 </p>
             </div>
-            <div className="flex md:hidden justify-between items-center w-full">
-                {/* Small devices */}
-                <Image src={ecobankLogo} alt="ecobank logo" height={32.5} />
-                <Image src={soudhuLogo} alt="soudhu logo" height={32.5} />
-                <Image src={ubaLogo} alt="uba logo" height={65} />
-                <Image src={codifyLogo} alt="codify logo" height={47.5} />
-                <Image src={odeytech} alt="odeytech logo" height={32.5} />
-                {/* <Image src={orange_money} alt="orange money icon" height={25} />
+            <div className="flex w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+                <motion.div
+                    className="flex flex-none md:hidden items-center gap-16 pr-16"
+                    animate={{
+                        translateX: "-50%",
+                    }}
+                    transition={{
+                        duration: 15,
+                        ease: "linear",
+                        repeat: Infinity,
+                        repeatType: "loop",
+                    }}
+                >
+                    {/* Small devices */}
+                    <Image src={ecobankLogo} alt="ecobank logo" height={32.5} />
+                    <Image src={soudhuLogo} alt="soudhu logo" height={32.5} />
+                    <Image src={ubaLogo} alt="uba logo" height={65} />
+                    <Image src={codifyLogo} alt="codify logo" height={47.5} />
+                    <Image src={odeytech} alt="odeytech logo" height={32.5} />
+
+                    {/* Second set of logos for animation */}
+                    <Image src={ecobankLogo} alt="ecobank logo" height={32.5} />
+                    <Image src={soudhuLogo} alt="soudhu logo" height={32.5} />
+                    <Image src={ubaLogo} alt="uba logo" height={65} />
+                    <Image src={codifyLogo} alt="codify logo" height={47.5} />
+                    <Image src={odeytech} alt="odeytech logo" height={32.5} />
+                    {/* <Image src={orange_money} alt="orange money icon" height={25} />
                 <Image src={wave} alt="wave icon" height={40} />
                 <Image src={moov_money} alt="moov money icon" height={40} />
                 <Image src={mtn_momo} alt="mtn momo icon" height={35} />
                 <Image src={free_money} alt="free money icon" height={62.5} />
                 <Image src={tmoney} alt="tmoney icon" height={37.5} /> */}
-            </div>
-            <div className="hidden md:flex justify-between items-center w-full">
-                {/* Large devices */}
-                <Image src={ecobankLogo} alt="ecobank logo" height={65} />
-                <Image src={soudhuLogo} alt="soudhu logo" height={65} />
-                <Image src={ubaLogo} alt="uba logo" height={130} />
-                <Image src={codifyLogo} alt="codify logo" height={95} />
-                <Image src={odeytech} alt="odeytech logo" height={65} />
-                {/* <Image src={orange_money} alt="orange money icon" height={55} /> */}
-                {/* <Image src={wave} alt="wave icon" height={80} /> */}
-                {/* <Image src={moov_money} alt="moov money icon" height={80} /> */}
-                {/* <Image src={mtn_momo} alt="mtn momo icon" height={70} /> */}
-                {/* <Image src={free_money} alt="free money icon" height={125} /> */}
-                {/* <Image src={tmoney} alt="tmoney icon" height={75} /> */}
+                </motion.div>
+                <motion.div
+                    className="hidden md:flex md:flex-none items-center gap-40 pr-40"
+                    animate={{
+                        translateX: "-50%",
+                    }}
+                    transition={{
+                        duration: 15,
+                        repeat: Infinity,
+                        ease: "linear",
+                        repeatType: "loop",
+                    }}
+                >
+                    {/* Large devices */}
+                    <Image src={ecobankLogo} alt="ecobank logo" height={65} />
+                    <Image src={soudhuLogo} alt="soudhu logo" height={65} />
+                    <Image src={ubaLogo} alt="uba logo" height={130} />
+                    <Image src={codifyLogo} alt="codify logo" height={95} />
+                    <Image src={odeytech} alt="odeytech logo" height={65} />
+
+                    {/* Second set of logos for animation */}
+                    <Image src={ecobankLogo} alt="ecobank logo" height={65} />
+                    <Image src={soudhuLogo} alt="soudhu logo" height={65} />
+                    <Image src={ubaLogo} alt="uba logo" height={130} />
+                    <Image src={codifyLogo} alt="codify logo" height={95} />
+                    <Image src={odeytech} alt="odeytech logo" height={65} />
+                    {/* <Image src={orange_money} alt="orange money icon" height={55} /> */}
+                    {/* <Image src={wave} alt="wave icon" height={80} /> */}
+                    {/* <Image src={moov_money} alt="moov money icon" height={80} /> */}
+                    {/* <Image src={mtn_momo} alt="mtn momo icon" height={70} /> */}
+                    {/* <Image src={free_money} alt="free money icon" height={125} /> */}
+                    {/* <Image src={tmoney} alt="tmoney icon" height={75} /> */}
+                </motion.div>
             </div>
         </section>
     );
