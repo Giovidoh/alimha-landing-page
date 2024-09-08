237b8e84c71a2029601248f1b110fe1b391792f4
giovanni <giovidoh@gmail.com>
2024-09-08 18:04:12 +0000

    feat: Alpay WhyUsSection internationalization en & fr

--- components/alpay/sections/WhyUsSection.tsx
+++ components/alpay/sections/WhyUsSection.tsx
@@ -8,8 +8,10 @@ import linkedLines from "@/public/assets/linked-line-vectors.png";
 import FlexibilityIcon from "@/public/assets/flexibility-icon.svg";
 import InnovationIcon from "@/public/assets/innovation-icon.svg";
 import ClientSatisfactionIcon from "@/public/assets/client-satisfaction-icon.svg";
+import { useTranslations } from "next-intl";
 
 const WhyUsSection = () => {
+    const t = useTranslations("AlpayPage.WhyUsSection");
     return (
         <section
             id="why-us"
@@ -22,31 +24,31 @@ const WhyUsSection = () => {
                     width={1500}
                 />
             </div>
-            <div className="flex flex-col justify-center items-center min-[1000px]:items-start w-full min-[1000px]:w-1/2 min-h-[600px] gap-4">
+            <div className="flex flex-col justify-center items-center min-[1000px]:items-start w-full min-[1000px]:w-1/2 min-h-[600px] gap-4 z-10">
                 <span className="font-medium text-base sm:text-lg text-primary-blue">
-                    Pourquoi-nous ?
+                    {t("tag")}
                 </span>
                 <h1 className="font-bold text-2xl sm:text-3xl min-[1000px]:text-4xl text-center min-[1000px]:text-start">
-                    Pourquoi Choisir Notre Agrégateur
+                    {t("heading")}
                 </h1>
                 <p className="text-sm text-[#292D32] mt-2 mb-5">
-                    Vous devez choisir Alpay pour :
+                    {t("subheading")}
                 </p>
                 <div className="flex flex-col gap-8">
                     <LeftIconTextCard
                         icon={<FlexibilityIcon />}
-                        title="Flexibilité et Compatibilité"
-                        body="Notre agrégateur s’adapte à tous les environnements de paiement, vous offrant une flexibilité inégalée pour gérer vos transactions."
+                        title={t("card1.title")}
+                        body={t("card1.body")}
                     />
                     <LeftIconTextCard
                         icon={<InnovationIcon />}
-                        title="Innovation Continue"
-                        body="Nous mettons à jour nos technologies pour répondre aux exigences évolutives du marché, vous offrant toujours les meilleures solutions disponibles."
+                        title={t("card2.title")}
+                        body={t("card2.body")}
                     />
                     <LeftIconTextCard
                         icon={<ClientSatisfactionIcon />}
-                        title="Satisfaction Client Garantie"
-                        body="Nos clients bénéficient d'une satisfaction élevée grâce à notre approche centrée sur leurs besoins et à notre support réactif."
+                        title={t("card3.title")}
+                        body={t("card3.body")}
                     />
                 </div>
             </div>
