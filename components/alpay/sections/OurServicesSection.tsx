cdc30eac28735f05475506c1bebc2a16098f7a80
giovanni <giovidoh@gmail.com>
2024-09-08 17:37:37 +0000

    feat: Alpay OurServicesSection internationalization en & fr

--- components/alpay/sections/OurServicesSection.tsx
+++ components/alpay/sections/OurServicesSection.tsx
@@ -4,25 +4,27 @@ import PaymentMethodProtectionIcon from "@/public/assets/payment-method-protecti
 import UniversalIntegrationIcon from "@/public/assets/universal-integration-icon.svg";
 import AdvancedSecurityIcon2 from "@/public/assets/advanced-security-icon2.svg";
 import ArrowRightIcon from "@/public/assets/arrow-right-icon.svg";
+import { useTranslations } from "next-intl";
 
 const OurServicesSection = () => {
+    const t = useTranslations("AlpayPage.OurServicesSection");
     return (
         <section
             id="our-services"
             className="flex flex-col items-center w-full py-24 px-5 sm:px-20 min-[1330px]:px-40 gap-3"
         >
             <span className="font-medium text-base sm:text-lg text-center text-primary-blue">
-                Nos Services
+                {t("tag")}
             </span>
-            <h1 className="font-bold text-2xl sm:text-3xl min-[1000px]:text-4xl text-center mb-5 sm:mb-10">
-                Ce Que Nous Offrons
-            </h1>
+            <h2 className="font-bold text-2xl sm:text-3xl min-[1000px]:text-4xl text-center mb-5 sm:mb-10">
+                {t("heading")}
+            </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                 <IconTextButtonCard
                     icon={<PaymentMethodProtectionIcon />}
-                    title="Consolidation des Moyens de Paiement"
-                    body="Réunissez divers modes de paiement en une seule interface. Acceptez des cartes de crédit, paiements mobiles, virements bancaires, et plus encore, tout en gardant une vue d'ensemble de vos flux financiers."
-                    buttonText="En Savoir Plus"
+                    title={t("card1.title")}
+                    body={t("card1.body")}
+                    buttonText={t("Learn more")}
                     buttonIcon={<ArrowRightIcon />}
                     className="bg-white border border-tertiary"
                     titleClassName="text-primary-blue"
@@ -31,9 +33,9 @@ const OurServicesSection = () => {
                 />
                 <IconTextButtonCard
                     icon={<UniversalIntegrationIcon className="mb-2" />}
-                    title="Intégration Universelle"
-                    body="Notre solution s'intègre facilement avec vos systèmes existants, que ce soit votre site web, votre application mobile ou votre logiciel de gestion. Bénéficiez d'une configuration rapide sans interruption de service."
-                    buttonText="En Savoir Plus"
+                    title={t("card2.title")}
+                    body={t("card2.body")}
+                    buttonText={t("Learn more")}
                     buttonIcon={<ArrowRightIcon />}
                     className="bg-primary-blue border border-primary-blue"
                     titleClassName="text-white"
@@ -42,9 +44,9 @@ const OurServicesSection = () => {
                 />
                 <IconTextButtonCard
                     icon={<AdvancedSecurityIcon2 />}
-                    title="Sécurité Avancée"
-                    body="Protégez vos transactions avec des normes de sécurité de pointe, incluant le chiffrement des données et la conformité PCI-DSS. Vos informations financières et celles de vos clients sont toujours en sécurité."
-                    buttonText="En Savoir Plus"
+                    title={t("card3.title")}
+                    body={t("card3.body")}
+                    buttonText={t("Learn more")}
                     buttonIcon={<ArrowRightIcon />}
                     className="bg-white border border-tertiary"
                     titleClassName="text-primary-blue"
