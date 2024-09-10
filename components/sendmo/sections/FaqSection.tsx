76bca15e0b1172c9e9c5c52abf46ad8a3c5e3f24
giovanni <giovidoh@gmail.com>
2024-09-10 05:30:41 +0000

    feat: Sendmo FaqSection internationalization en & fr

--- components/sendmo/sections/FaqSection.tsx
+++ components/sendmo/sections/FaqSection.tsx
@@ -1,43 +1,45 @@
 import React from "react";
 import Badge from "../badges/Badge";
 import Accordion from "../Accordion";
-
-const accordionInfos = [
-    {
-        title: "Comment ouvrir un compte Sendmo ?",
-        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
-    },
-    {
-        title: "Sendmo est-elle facile à comprendre et à utiliser ?",
-        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
-    },
-    {
-        title: "Les utilisateurs de Sendmo ont-ils accès à un service d'assistance à la clientèle ?",
-        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
-    },
-    {
-        title: "Comment démarrer avec sendmo ?",
-        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
-    },
-    {
-        title: "Comment démarrer avec sendmo ?",
-        body: "Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.Lorem ipsum dolor sit amet consectetur. Erat turpis id egestas facilisi. Egestas justo tellus pharetra egestas.",
-    },
-];
+import { useTranslations } from "next-intl";
 
 const FaqSection = () => {
+    const t = useTranslations("SendmoPage.FaqSection");
+    const accordionInfos = [
+        {
+            title: t("Accordion.question1.title"),
+            body: t("Accordion.question1.body"),
+        },
+        {
+            title: t("Accordion.question2.title"),
+            body: t("Accordion.question2.body"),
+        },
+        {
+            title: t("Accordion.question3.title"),
+            body: t("Accordion.question3.body"),
+        },
+        {
+            title: t("Accordion.question4.title"),
+            body: t("Accordion.question4.body"),
+        },
+        {
+            title: t("Accordion.question5.title"),
+            body: t("Accordion.question5.body"),
+        },
+    ];
+
     return (
         <section
             id="faq_section"
             className="flex flex-col justify-between items-center gap-6 sm:gap-10 px-5 sm:px-20 min-[1120px]:px-40 py-20"
         >
-            <Badge text="FAQS" px="px-14" />
+            <Badge text={t("tag")} px="px-14" />
             <div className="flex flex-col items-center gap-3">
                 <h2 className="text-2xl md:text-4xl text-center leading-snug font-bold tracking-wide">
-                    Avez-vous des questions?
+                    {t("heading")}
                 </h2>
                 <p className="text-gray-1 text-center text-pretty font-light">
-                    Quelques questions utiles pour l’utilisation de Sendmo.
+                    {t("subheading")}
                 </p>
             </div>
             <div className="flex flex-col w-[95%] sm:w-[80%] gap-4">
