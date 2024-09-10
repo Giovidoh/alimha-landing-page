a8ff33588a0ae29e0e36f3737c7594af6a359d4e
giovanni <giovidoh@gmail.com>
2024-09-10 04:45:52 +0000

    feat: Sendmo FeaturesSection internationalization en & fr

--- components/sendmo/sections/FeaturesSection.tsx
+++ components/sendmo/sections/FeaturesSection.tsx
@@ -9,47 +9,49 @@ import RealTimeTransactionTrackingIcon from "@/public/assets/real-time-transacti
 import realTimeTransactionTrackingIllustration from "@/public/assets/real-time-transaction-tracking-illustration.png";
 import ClientSupportIcon from "@/public/assets/client-support-24-7-icon.svg";
 import clientSupportIllustration from "@/public/assets/client-support-24-7-illustration.png";
+import { useTranslations } from "next-intl";
 
 const FeaturesSection = () => {
+    const t = useTranslations("SendmoPage.FeaturesSection");
     return (
         <section
             id="features_section"
             className="flex flex-col items-center px-5 sm:px-20 min-[1120px]:px-40 pt-10 md:pt-20 py-36 gap-10"
         >
-            <Badge text="Fonctionnalités" />
-            <h1 className="text-2xl md:text-4xl text-center md:text-start leading-snug font-bold tracking-wide">
-                La Solution Innovante pour <br className="hidden md:block" />{" "}
-                Vos Dépôts Mobile Money
+            <Badge text={t("tag")} />
+            <h1 className="text-2xl md:text-4xl text-center leading-snug font-bold tracking-wide">
+                {t("heading.first")} <br className="hidden md:block" />{" "}
+                {t("heading.second")}
             </h1>
             <div className="grid min-[1300px]:grid-cols-2 gap-2">
                 <IconTextIllustrationCard
                     backgroundColor="bg-secondary-blue"
                     icon={<InstantDepositIcon />}
-                    title="Dépôts Instantanés"
-                    body="Envoyez de l'Argent en Quelques Secondes. Profitez de la rapidité et de la simplicité de nos dépôts instantanés, adaptés à vos besoins quotidiens"
+                    title={t("card1.title")}
+                    body={t("card1.description")}
                     illustration={instantDepositIllustration}
                 />
                 <IconTextIllustrationCard
                     backgroundColor="bg-gray-50"
                     foregroundColor="text-black"
                     icon={<SubRegionalTransfersIcon />}
-                    title="Transferts Sous-Régionaux"
-                    body="Une Connectivité Sans Frontières, Réalisez des transferts entre le Sénégal, la Guinée, le Togo, la Centrafrique, Djibouti et les Comores. SendMo vous offre une couverture sous-régionale inégalée."
+                    title={t("card2.title")}
+                    body={t("card2.description")}
                     illustration={subRegionalTransfersIllustration}
                 />
                 <IconTextIllustrationCard
                     backgroundColor="bg-gray-50"
                     foregroundColor="text-black"
                     icon={<RealTimeTransactionTrackingIcon />}
-                    title="Suivi des Transactions en Temps Réel"
-                    body="Gardez le Contrôle de Vos Finances, Recevez des notifications instantanées et accédez à un historique détaillé de toutes vos transactions directement depuis l'application."
+                    title={t("card3.title")}
+                    body={t("card3.description")}
                     illustration={realTimeTransactionTrackingIllustration}
                 />
                 <IconTextIllustrationCard
                     backgroundColor="bg-secondary-blue"
                     icon={<ClientSupportIcon />}
-                    title="Support Client 24/7"
-                    body="Assistance à Tout Moment, Notre équipe dédiée est toujours prête à vous aider, assurant une expérience utilisateur sans faille."
+                    title={t("card4.title")}
+                    body={t("card4.description")}
                     illustration={clientSupportIllustration}
                 />
             </div>
