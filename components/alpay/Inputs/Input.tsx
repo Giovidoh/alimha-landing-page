a651ac13a3f729b8d8e2b01537ebce8e5b1d5194
giovanni <giovidoh@gmail.com>
2024-09-10 03:10:36 +0000

    fix: Alpay contactForm: requiring data in fields.

--- components/alpay/Inputs/Input.tsx
+++ components/alpay/Inputs/Input.tsx
@@ -17,6 +17,7 @@ const Input: React.FC<InputProps> = ({
             name={name}
             placeholder={placeholder}
             className="bg-[#F8FAFC] sm:text-xl text-[#8E91B5] border border-[#EDEDED] w-full px-5 py-2 sm:py-3 rounded-lg outline-none"
+            required
         />
     );
 };
--- components/alpay/Inputs/Textarea.tsx
+++ components/alpay/Inputs/Textarea.tsx
@@ -15,6 +15,7 @@ const TextArea: React.FC<TextAreaProps> = ({
             id=""
             placeholder={placeholder}
             className="resize-none bg-[#F8FAFC] sm:text-xl text-[#8E91B5] border border-[#EDEDED] w-full h-[110px] sm:h-[214px] px-5 py-2 sm:py-3 rounded-lg outline-none"
+            required
         ></textarea>
     );
 };
